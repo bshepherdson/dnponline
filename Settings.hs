1e92ac4d10b11efb30206f8997cd129a232ccf50
Braden Scott Shepherdson <bsshephe@strombola.csclub.uwaterloo.ca>
2010-12-12 23:46:41 -0500

    Revert "Local change: Hostname"
    This reverts commit eae4c8aa443a6a24b16d857e4565c2fc8ce32de5.

--- Settings.hs
+++ Settings.hs
@@ -39,7 +39,7 @@ approot :: String
 -- Please note that there is no trailing slash.
 approot = "http://localhost:3000"
 #else
-approot = "http://strombola.csclub.uwaterloo.ca:3000"
+approot = "http://localhost:3000"
 #endif
 
 -- | The location of static files on your system. This is a file system
