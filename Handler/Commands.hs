5e25914b0f6d71de6461ff9eb418a1d632a503e5
Braden Shepherdson <Braden.Shepherdson@gmail.com>
2010-12-13 02:00:02 -0500

    Add /roll command

--- Handler/Commands.hs
+++ Handler/Commands.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell, OverloadedStrings, NoMonomorphismRestriction #-}
 module Handler.Commands (
     commandMap
   , CommandResponse (..)
@@ -15,11 +15,28 @@ import Control.Concurrent.STM
 import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TChan
 
+import System.Random
+import Text.Parsec
+
+import Data.List
+
+sum' = foldl' (+) 0
 
 commandMap = M.fromList [ ("nick", cmdNick)
                         , ("host", cmdHost)
                         , ("join", cmdJoin)
                         , ("debug", cmdDebug)
+                        , ("roll", cmdRoll)
+                        , ("r",    cmdRoll)
+                        , ("d3",   cmdRoll)
+                        , ("d4",   cmdRoll)
+                        , ("d6",   cmdRoll)
+                        , ("d8",   cmdRoll)
+                        , ("d10",  cmdRoll)
+                        , ("d12",  cmdRoll)
+                        , ("d20",  cmdRoll)
+                        , ("d100", cmdRoll)
+                        , ("d%",   cmdRoll)
                         ]
 
 -- user id, user, nick, command, args
@@ -85,4 +102,42 @@ cmdDebug _ _ _ _ _ = do
   liftIO $ print uts
   liftIO $ print (M.map (\(Table cs p) -> M.keys cs) ts)
   return $ ResponseSuccess
+
+
+
+syntaxRoll :: String
+syntaxRoll = "Syntax: /roll AdB+C\nExamples: /roll 2d6-2   /roll d20   /roll 1d8+3"
+
+cmdRoll :: Command
+cmdRoll uid u nick "d3"   _ = cmdRoll uid u nick "roll" ["1d3"]
+cmdRoll uid u nick "d4"   _ = cmdRoll uid u nick "roll" ["1d4"]
+cmdRoll uid u nick "d6"   _ = cmdRoll uid u nick "roll" ["1d6"]
+cmdRoll uid u nick "d8"   _ = cmdRoll uid u nick "roll" ["1d8"]
+cmdRoll uid u nick "d10"  _ = cmdRoll uid u nick "roll" ["1d10"]
+cmdRoll uid u nick "d12"  _ = cmdRoll uid u nick "roll" ["1d12"]
+cmdRoll uid u nick "d20"  _ = cmdRoll uid u nick "roll" ["1d20"]
+cmdRoll uid u nick "d100" _ = cmdRoll uid u nick "roll" ["1d100"]
+cmdRoll uid u nick "d%"   _ = cmdRoll uid u nick "roll" ["1d100"]
+cmdRoll uid u nick _ []     = return $ ResponsePrivate syntaxRoll
+cmdRoll uid u nick _ (x:_)  = do
+    case parse parseDice "" (case x of ('d':_) -> '1':x; _ -> x) of
+      Left _ -> return $ ResponsePrivate syntaxRoll
+      Right (a,b,c) | a <= 0 -> return $ ResponsePrivate "Number of dice cannot be less than 1."
+                    | b <= 0 -> return $ ResponsePrivate "Size of dice cannot be less than 1."
+                    | otherwise -> do
+                        rolls <- replicateM a $ liftIO (randomRIO (1,b))
+                        let total = sum' rolls + c
+                        send uid serverName $ nick ++ " rolled " ++ show a ++ "d" ++ show b ++ (if c < 0 then show c else "+" ++ show c) ++ " and got " ++ show total
+  where parseDice = do
+          a <- many1 digit
+          char 'd'
+          b <- many1 digit
+          mc1 <- optionMaybe $ char '+' >> many1 digit
+          mc2 <- optionMaybe $ char '-' >> many1 digit
+          let c = case (mc1,mc2) of
+                    (Nothing,Nothing) -> 0
+                    (Just x, Nothing) -> read x
+                    (Nothing,Just y)  -> read y
+          return ((read a, read b, c) :: (Int,Int,Int))
+
 
